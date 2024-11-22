c4f4f60853866aa0da46ddf162c95615bf500d04
nufaz867! <2223609.nurul@smkn-2sbg.sch.id>
2024-11-22 14:10:46 +0700

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+ 
+class Post extends Model
+{    
+    /**
+     * index
+     *
+     * @return void
+     */
+    public function index()
+    {
+        //get posts
+        $posts = Post::latest()->paginate(5);
+
+        //render view with posts
+        return view('posts.index', compact('posts'));
+    }
+}
